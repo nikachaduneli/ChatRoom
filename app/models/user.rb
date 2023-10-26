ad9c21a9cc86fa6bd207dcfd6b2db66e6bd50def
nikachaduneli <chaduneli@hmg.software>
2023-10-26 16:45:55 +0400

    message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 20, minimum: 4}
   has_secure_password
-  validates :username, presence: true, uniqueness: {case_sensitive: true}, length: {maximum: 20, minimum: 4}
+  has_many :messages
 end
